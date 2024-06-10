c90a425ab3f3f809dfa6556fed3f242645a0e33c
MeriMkrtchyann <142087396+MeriMkrtchyann@users.noreply.github.com>
2024-06-10 11:22:08 +0400

    Delete LoginPageHookToken directory

--- LoginPageHookToken/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import activeUserReducer from "../redux/slices/activeUser/activeUserSlice"
-
-export const store = configureStore({
-  reducer: {
-    activeUser :activeUserReducer
-  },
-})
